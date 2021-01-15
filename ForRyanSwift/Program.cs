6bcaffcc4c779df44e5388b76d18379858d0179a
Adam Drummond <adam.drummond9@gmail.com>
2021-01-15 14:02:20 +0000

    first-commit

---
+++ ForRyanSwift/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ForRyanSwift
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This program is for Ryan Swift");
+
+            Console.WriteLine("How to get to the Isle of Man:");
+            Console.WriteLine("Press any key to show the following steps");
+
+            Console.WriteLine("1: Go to your local airport");
+            Console.ReadLine();
+
+            Console.WriteLine("2: Fly to an airport that has flights to LHR or MAN");
+            Console.ReadLine();
+
+            Console.WriteLine("3: Fly to LHR or MAN");
+            Console.ReadLine();
+
+            Console.WriteLine("4: Pass through immigration");
+            Console.ReadLine();
+
+            Console.WriteLine("5: Transfer onto a Loganair flight to the Isle of Man");
+            Console.ReadLine();
+
+            Console.WriteLine("Btw you need an exemption certificate to get in...");
+
+            Console.WriteLine("end of instructions");
+        }
+    }
+}
